935da84321ad6d1a359125b22efa3ad52a314c4d
heshanth123 <heshanmegan@gmail.com>
2025-10-26 23:11:02 +0530

    Added UserRole file

---
+++ src/main/java/org/example/Food/model/UserRole.java
@@ -0,0 +1,10 @@
+package org.example.model;
+
+// Member: Heshan - User Registration and Customer management
+// Related database tables: user & customer tables
+
+public enum UserRole {
+    ADMIN,
+    SHOP,
+    CUSTOMER
+}
