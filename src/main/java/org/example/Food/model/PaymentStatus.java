f83480f70dce0b67afc6d9d9b2706a96af064517
APiranavan <arumainayagampiranavan@gmail.com>
2025-10-27 06:05:36 +0530

    added PaymentStatus

---
+++ src/main/java/org/example/Food/model/PaymentStatus.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+// Member: Piranavan - Order management
+// Related database tables: orders and order item tables
+
+public enum PaymentStatus {
+    PENDING,
+    PAID,
+    FAILED,
+    REFUNDED
+}
